206af63a35cf3d4a1b84a3429485f13fad97c526
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-10-15 03:37:15 -0600

    feat: llega hasta el final de la matriz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from "rxjs";
+import { MatGridTile } from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('changeState', [
-      state('state1', style({
-        backgroundColor: 'green',
-        transform: 'scale(1)'
-      })),
-      state('state2', style({
-        background: "url('../assets/spider.png')",
-        backgroundRepeat: 'no-repeat'
-      })),
-      transition('*=>state1', animate('300ms')),
-      transition('*=>state2', animate('2000ms'))
-    ])
   ]
 })
 export class AppComponent implements OnInit {
-  toState = 'state1';
+  state: Array<string> = [];
   values: Array<number> = [];
   title = 'P3Lenguajes';
+  interval;
 
   ngOnInit() {
     for (let i = 1; i <= 36; i++) {
@@ -30,8 +20,26 @@ export class AppComponent implements OnInit {
     }
   }
 
-  changeState(state: any) {
-    this.toState = state;
-    console.log(this.toState);
+  start(tile, index: number){
+    console.log(tile)
+    let count = index-1;
+    this.interval = setInterval(() => {
+      count++;
+      if(count === this.values.length) {
+        clearInterval(this.interval);
+        return;
+      };
+      let style = document.createElement("style");
+      style.type = "text/css";
+      style.innerHTML =
+      ".background { background-image: url('../assets/spider.png'); background-repeat: no-repeat}";
+      document.getElementsByTagName("head")[0].appendChild(style);
+      let prev = document.getElementById(`${count-1}`);
+      prev.classList.remove("background")
+
+      let next = document.getElementById(`${count}`);
+      //console.log(next)
+      next.classList.add("background");
+    },1000)
   }
 }
