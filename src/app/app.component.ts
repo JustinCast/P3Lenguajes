5f3643b4db4512f40c6f3ef5d5871b1cdcfb612d
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-10-15 02:02:30 -0600

    feat: animaciones básicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('changeState', [
+      state('state1', style({
+        backgroundColor: 'green',
+        transform: 'scale(1)'
+      })),
+      state('state2', style({
+        backgroundColor: 'red',
+        transform: 'scale(1.5)'
+      })),
+      transition('*=>state1', animate('300ms')),
+      transition('*=>state2', animate('2000ms'))
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
+  toState = 'state1';
   values: Array<number> = [];
   title = 'P3Lenguajes';
 
@@ -13,5 +28,10 @@ export class AppComponent implements OnInit {
     for (let i = 1; i <= 36; i++) {
       this.values.push(i);
     }
+  }
+
+  changeState(state: any) {
+    this.toState = state;
+    console.log(this.toState);
   }
 }
