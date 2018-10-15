b3e8f5296a7be1a2228c48ac3268cbec06aa764e
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-10-15 01:45:49 -0600

    feat: minimal styled changes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatGridListModule } from "@angular/material";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatGridListModule, MatButtonModule, MatIconModule } from "@angular/material";
 
 @NgModule({
-  imports: [
-    CommonModule, MatGridListModule
-  ],
-  exports: [
-    MatGridListModule
-  ],
+  imports: [CommonModule, MatGridListModule, MatButtonModule, MatIconModule],
+  exports: [MatGridListModule, MatButtonModule, MatIconModule],
   declarations: []
 })
-export class MaterialModule { }
+export class MaterialModule {}
