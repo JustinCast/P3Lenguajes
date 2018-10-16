03337cd4bf8a632731ad74f44159bc630d48e95c
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-10-16 03:26:09 -0600

    feat: cambios importantes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,78 @@
 package sample;
 
+import javafx.event.Event;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import org.jpl7.Query;
+import org.jpl7.Term;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Controller {
+    GridPane buttonsContainer;
+    private boolean spider = false;
+    private boolean bee = false;
+    private ArrayList route = new ArrayList();
+
+    public Controller() {
+        initializeProlog();
+    }
+
+    public void onBtnClick(Event e) {
+
+    }
+    public void onStartGameClick(Event e) {
+        for(Object o : this.route)
+            System.out.println(o);
+    }
+
+    private void initializeProlog() {
+        String PROLOG = "consult('ScriptProlog.pl')";
+        Query query = new Query(PROLOG);
+        if (query.hasSolution()) {
+            addNodes();
+
+            String shortest = "find_paths(35, 0)";
+            Query q4 = new Query(shortest);
+            System.out.println(q4.oneSolution().toString());
+            //String[] routeSplitted = route.split(",");
+            //this.route.addAll(Arrays.asList(routeSplitted));
+        } else {
+            JOptionPane.showMessageDialog(null, "!!!No se pudo conectar con Prolog!!!");
+        }
+    }
+
+    private void addNodes() {
+        for(int i = 0; i < 35; i++) {
+
+            // agrega hacia adelante y hacia atras
+            int randomNum = ThreadLocalRandom.current().nextInt(0, 10 + 1);
+            String query = "add("+ i +", " + (i + 1) + ", " + randomNum + ")";
+            String reverseQuery = "add("+ (i + 1) +", " + i + ", " + randomNum + ")";
+            Query consult = new Query(query); consult.oneSolution(); consult.close();
+            Query reverseConsult = new Query(reverseQuery); reverseConsult.oneSolution(); reverseConsult.close();
+
+            // agrega hacia arriba y hacia abajo, con las excepciones
+            if(i <= 9){
+                query = "add("+ i +", " + (i + 6) + ", " + randomNum + ")";
+                consult = new Query(query); consult.oneSolution(); consult.close();
+            }
+            else if(i > 29) {
+                query = "add("+ i +", " + (i - 6) + ", " + randomNum + ")";
+                consult = new Query(query); consult.oneSolution(); consult.close();
+            }
+            else {
+                query = "add("+ i +", " + (i + 6) + ", " + randomNum + ")";
+                reverseQuery = "add("+ i +", " + (i - 6)  + ", " + randomNum + ")";
+                consult = new Query(query); consult.oneSolution(); consult.close();
+                reverseConsult = new Query(reverseQuery); reverseConsult.oneSolution(); reverseConsult.close();
+            }
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,18 +18,6 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
-        String PROLOG = "consult('ScriptProlog.pl')";
-        Query query = new Query(PROLOG);
-        if (query.hasSolution()) {
-            addNodes();
-
-            String prueba = "find_paths(35, 0)";
-            Query q4 = new Query(prueba);
-            System.out.println(q4.oneSolution()); q4.close();
-        } else {
-            JOptionPane.showMessageDialog(null, "!!!No se pudo conectar con Prolog!!!");
-        }
     }
 
 
@@ -37,31 +25,4 @@ public class Main extends Application {
         launch(args);
     }
 
-    private void addNodes() {
-        for(int i = 0; i < 35; i++) {
-
-            // agrega hacia adelante y hacia atras
-            int randomNum = ThreadLocalRandom.current().nextInt(0, 10 + 1);
-            String query = "add("+ i +", " + (i + 1) + ", " + randomNum + ")";
-            String reverseQuery = "add("+ (i + 1) +", " + i + ", " + randomNum + ")";
-            Query consult = new Query(query); consult.oneSolution(); consult.close();
-            Query reverseConsult = new Query(reverseQuery); reverseConsult.oneSolution(); reverseConsult.close();
-
-            // agrega hacia arriba y hacia abajo, con las excepciones
-            if(i <= 9){
-                query = "add("+ i +", " + (i + 6) + ", " + randomNum + ")";
-                consult = new Query(query); consult.oneSolution(); consult.close();
-            }
-            else if(i > 29) {
-                query = "add("+ i +", " + (i - 6) + ", " + randomNum + ")";
-                consult = new Query(query); consult.oneSolution(); consult.close();
-            }
-            else {
-                query = "add("+ i +", " + (i + 6) + ", " + randomNum + ")";
-                reverseQuery = "add("+ i +", " + (i - 6)  + ", " + randomNum + ")";
-                consult = new Query(query); consult.oneSolution(); consult.close();
-                reverseConsult = new Query(reverseQuery); reverseConsult.oneSolution(); reverseConsult.close();
-            }
-        }
-    }
 }
